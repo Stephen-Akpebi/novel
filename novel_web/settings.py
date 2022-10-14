1820c05d74f0aa2775eee3fb986aa480995e809a
Stephen-Akpebi <www.nineclouds@gmail.com>
2022-10-14 05:59:03 +0000

    novel for heroku

--- novel_web/settings.py
+++ novel_web/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'novel',
+    'active_link',
     #'debug_toolbar',
     #"django_bootstrap5",
     #'crispy_forms',
